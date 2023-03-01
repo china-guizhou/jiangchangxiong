85bdd0552d26786e4ea0ee304a27248562dc75e9
Jiang <cutuopen@aliyun.com>
2023-03-01 23:26:02 +0800

    20230301//new//init commit

---
+++ bootstrap/src/main/java/com/jiangchangxiong/AppBootstrap.java
@@ -0,0 +1,13 @@
+package com.jiangchangxiong;
+
+/**
+ * @author Jiang
+ * @since 2023-03-01
+ */
+public class AppBootstrap {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ core/src/main/java/com/jiangchangxiong/utils/StrUtils.java
@@ -0,0 +1,12 @@
+package com.jiangchangxiong.utils;
+
+import cn.hutool.core.util.StrUtil;
+import lombok.experimental.UtilityClass;
+
+/**
+ * @author Jiang
+ * @since  2023-03-01
+ */
+@UtilityClass
+public class StrUtils extends StrUtil {
+}
