fccf1c82f4df57ee34d8b8ec7e22b28ad49c496c
Jiang <cutuopen@aliyun.com>
2023-03-03 00:03:53 +0800

    20230301//new//init commit

---
+++ modules/module-system/module-system-api/src/main/java/com/jiangchangxiong/module/system/entity/SysPay.java
@@ -0,0 +1,8 @@
+package com.jiangchangxiong.module.system.entity;
+
+/**
+ * @author Jiang
+ * @since 2023-03-02
+ */
+public class SysPay {
+}
--- modules/module-system/module-system-api/src/main/java/com/jiangchangxiong/module/system/entity/SysUser.java
+++ modules/module-system/module-system-api/src/main/java/com/jiangchangxiong/module/system/entity/SysUser.java
@@ -1,8 +1,16 @@
 package com.jiangchangxiong.module.system.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.jiangchangxiong.starter.mp.core.UpdateEntity;
+import lombok.Data;
+
 /**
+ * System User
+ *
  * @author Jiang
- * @since 2023-03-02
+ * @since  2023-03-02
  */
-public class SysUser {
+@Data
+@TableName("sys_user")
+public class SysUser extends UpdateEntity {
 }
