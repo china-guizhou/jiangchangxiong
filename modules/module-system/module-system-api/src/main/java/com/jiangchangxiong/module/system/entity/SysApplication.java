951c343dcf79f69a10ce92508cd286e4d04f7f3a
jiangchangxiong <cutuopen@aliyun.com>
2023-03-05 22:33:22 +0800

    20230305//new//complete code

--- modules/module-system/module-system-api/src/main/java/com/jiangchangxiong/module/system/entity/SysApplication.java
+++ modules/module-system/module-system-api/src/main/java/com/jiangchangxiong/module/system/entity/SysApplication.java
@@ -1,8 +1,36 @@
 package com.jiangchangxiong.module.system.entity;
 
+import com.baomidou.mybatisplus.annotation.TableLogic;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.jiangchangxiong.starter.mp.core.UpdateEntity;
+
+import lombok.Getter;
+import lombok.Setter;
+
 /**
- * @author Jiang
- * @since 2023-03-02
+ * @author jiangchangxiong
+ * @since  2023-03-02
  */
-public class SysApplication {
+@Getter
+@Setter
+@TableName("sys_application")
+public class SysApplication extends UpdateEntity {
+
+	private static final long serialVersionUID = -41212512411L;
+	
+	/** unique code */
+	private String code;
+	
+	private String name;
+	
+	/**  */
+	private String publicKey;
+	
+	private String privateKey;
+	
+	private Boolean enabled;
+	
+	@TableLogic(value = "0", delval = "1")
+	private Integer isDeleted;
+	
 }
